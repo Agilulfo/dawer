575a40208ce4a1945a6be45cea2a40901886ea05
Flavio Di Natale <flavio@ubnt.com>
2016-06-13 19:57:06 +0200

    Add get_collection_sub_directory method to Image

--- dawer/image.py
+++ dawer/image.py
@@ -1,6 +1,6 @@
 from os.path import exists, join
 
-from .exceptions import NotValidPath
+from .exceptions import NotValidPath, UnknownImageDate
 from .filesystem import get_images_in_folder, move_files_to_dir
 from .process import DateParser
 
@@ -15,14 +15,23 @@ class Image:
     def extract_date_from_name(self, dateparser):
         self.date = dateparser.get_date(self.filename)
 
+    def get_collection_sub_directory(self):
+        if self.date:
+            return '{year:04}/{month:02}/'.format(
+                year=self.date.year,
+                month=self.date.month
+            )
+        return None
+
     def move_to_collection(self, collection_path):
-        date_folder = '{year:04}/{month:02}/'.format(
-            year=self.date.year, month=self.date.month
-        )
-        destination = join(collection_path, date_folder)
-        source = join(self.directory, self.filename)
-        move_files_to_dir([source], destination)
-        self.directory = destination
+        if self.date:
+            sub_directory = self.get_collection_sub_directory()
+            destination = join(collection_path, sub_directory)
+            source = join(self.directory, self.filename)
+            move_files_to_dir([source], destination)
+            self.directory = destination
+        else:
+            raise UnknownImageDate(self)
 
 
 class ImagesHandler:
--- tests/test_images.py
+++ tests/test_images.py
@@ -1,8 +1,10 @@
 from datetime import date
 from os.path import join
 
+import pytest
 from mock import patch
 
+from dawer.exceptions import UnknownImageDate
 from dawer.image import Image, ImagesHandler
 from dawer.process import DateParser
 
@@ -31,6 +33,21 @@ def test_extract_date_from_unformatted_filename():
     assert image.date is None
 
 
+def test_get_collection_sub_directory():
+    source_base_path = './a'
+    image_name = '2016-05-12.jpg'
+
+    image = Image(source_base_path, image_name)
+    sub_directory = image.get_collection_sub_directory()
+
+    assert sub_directory is None
+
+    image.date = date(2016, 5, 12)
+    sub_directory = image.get_collection_sub_directory()
+
+    assert sub_directory == '2016/05/'
+
+
 @patch('dawer.image.move_files_to_dir')
 def test_move_image(move):
     source_base_path = './a'
@@ -46,6 +63,16 @@ def test_move_image(move):
 
     move.assert_called_with([source], destination)
     assert image.directory == destination
+
+
+def test_move_image_with_unknown_date():
+    source_base_path = './a'
+    collection_path = './b'
+    image_name = 'unknown.jpg'
+
+    image = Image(source_base_path, image_name)
+    with pytest.raises(UnknownImageDate):
+        image.move_to_collection(collection_path)
 
 
 @patch('dawer.image.get_images_in_folder')
